8ce55294388ef02bfc6e9edf7c50f75cef22039e
DiogenestheDog <rguthrie14@gmail.com>
2020-08-17 17:39:55 -0700

    webpack fun

--- dist/bundle.js
+++ dist/bundle.js
@@ -81,19 +81,19 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./src/app.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/app.js":
-/*!********************!*\
-  !*** ./src/app.js ***!
-  \********************/
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+console.log("I think I'm here");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: './src/app.js',
+    entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: './bundle.js'
